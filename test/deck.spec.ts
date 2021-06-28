4de635a6cebb5b0445472e11ecb6ebad6fe10c47
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-06-28 14:21:30 +0100

    Add first couple of unit tests re consistency of deck of cards

--- test/deck.spec.ts
+++ test/deck.spec.ts
@@ -1,4 +1,64 @@
+import { Deck } from '../src/deck'
+import { Colour, HexagoNumber, Rotation } from '../src/enums'
+
 describe('Deck tests', () => {
-  it('placeholder', () => {
+
+  describe('originalCards', () => {
+
+    it('should be a total of 36 wedges with each colour', () => {
+      let numRed = 0
+      let numGreen = 0
+      let numBlue = 0
+      let numYellow = 0
+      let numOrange = 0
+      let numPurple = 0
+      for (const card of Deck.originalCards) {
+        for (const wedgeIndex of [0, 1, 2, 3, 4, 5]) {
+          const wedge = card.wedgeAt(wedgeIndex, Rotation.Rotation0)
+          switch (wedge.colour) {
+            case Colour.Red: numRed++; break
+            case Colour.Green: numGreen++; break
+            case Colour.Blue: numBlue++; break
+            case Colour.Yellow: numYellow++; break
+            case Colour.Orange: numOrange++; break
+            case Colour.Purple: numPurple++; break
+          }
+        }
+      }
+      expect(numRed).toBe(36)
+      expect(numGreen).toBe(36)
+      expect(numBlue).toBe(36)
+      expect(numYellow).toBe(36)
+      expect(numOrange).toBe(36)
+      expect(numPurple).toBe(36)
+    })
+
+    it('should be a total of 36 wedges with each number', () => {
+      let numOne = 0
+      let numTwo = 0
+      let numThree = 0
+      let numFour = 0
+      let numFive = 0
+      let numSix = 0
+      for (const card of Deck.originalCards) {
+        for (const wedgeIndex of [0, 1, 2, 3, 4, 5]) {
+          const wedge = card.wedgeAt(wedgeIndex, Rotation.Rotation0)
+          switch (wedge.number) {
+            case HexagoNumber.Number1: numOne++; break
+            case HexagoNumber.Number2: numTwo++; break
+            case HexagoNumber.Number3: numThree++; break
+            case HexagoNumber.Number4: numFour++; break
+            case HexagoNumber.Number5: numFive++; break
+            case HexagoNumber.Number6: numSix++; break
+          }
+        }
+      }
+      expect(numOne).toBe(36)
+      expect(numTwo).toBe(36)
+      expect(numThree).toBe(36)
+      expect(numFour).toBe(36)
+      expect(numFive).toBe(36)
+      expect(numSix).toBe(36)
+    })
   })
 })
