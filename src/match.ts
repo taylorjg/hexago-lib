9cbb20e9a419c920eaed32d92ea27e85f1897047
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-07-17 12:52:31 +0100

    Add convenience properties to Match for wedge and otherWedge

--- src/match.ts
+++ src/match.ts
@@ -1,7 +1,10 @@
 import { PlacedCard } from './placedCard'
+import { Wedge } from './wedge'
 
 export class Match {
 
+  public readonly wedge: Wedge
+  public readonly otherWedge: Wedge
   public readonly coloursMatch: boolean
   public readonly numbersMatch: boolean
   public readonly score: number
@@ -12,11 +15,11 @@ export class Match {
     public readonly otherPlacedCard: PlacedCard,
     public readonly otherWedgeIndex: number,
   ) {
-    const wedge = this.placedCard.wedgeAt(this.wedgeIndex)
-    const otherWedge = this.otherPlacedCard.wedgeAt(this.otherWedgeIndex)
-    this.coloursMatch = wedge.colour == otherWedge.colour
-    this.numbersMatch = wedge.number == otherWedge.number
-    const sum = wedge.number + otherWedge.number
+    this.wedge = placedCard.wedgeAt(wedgeIndex)
+    this.otherWedge = otherPlacedCard.wedgeAt(otherWedgeIndex)
+    this.coloursMatch = this.wedge.colour == this.otherWedge.colour
+    this.numbersMatch = this.wedge.number == this.otherWedge.number
+    const sum = this.wedge.number + this.otherWedge.number
     const multiplier = (this.coloursMatch ? 1 : 0) + (this.numbersMatch ? 1 : 0)
     this.score = sum * multiplier
   }
