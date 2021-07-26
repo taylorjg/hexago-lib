733abd3d4c351787d610fcd460e565594f051a1a
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-07-26 22:51:15 +0100

    allRotations, rotationRotateCW, rotationRotateCCW

--- src/enums.ts
+++ src/enums.ts
@@ -24,3 +24,34 @@ export enum Rotation {
   Rotation240 = 4,
   Rotation300 = 5
 }
+
+export const allRotations = [
+  Rotation.Rotation0,
+  Rotation.Rotation60,
+  Rotation.Rotation120,
+  Rotation.Rotation180,
+  Rotation.Rotation240,
+  Rotation.Rotation300
+]
+
+export const rotationRotateCW = (rotation: Rotation): Rotation => {
+  switch (rotation) {
+    case Rotation.Rotation0: return Rotation.Rotation60
+    case Rotation.Rotation60: return Rotation.Rotation120
+    case Rotation.Rotation120: return Rotation.Rotation180
+    case Rotation.Rotation180: return Rotation.Rotation240
+    case Rotation.Rotation240: return Rotation.Rotation300
+    case Rotation.Rotation300: return Rotation.Rotation0
+  }
+}
+
+export const rotationRotateCCW = (rotation: Rotation): Rotation => {
+  switch (rotation) {
+    case Rotation.Rotation0: return Rotation.Rotation300
+    case Rotation.Rotation60: return Rotation.Rotation0
+    case Rotation.Rotation120: return Rotation.Rotation60
+    case Rotation.Rotation180: return Rotation.Rotation120
+    case Rotation.Rotation240: return Rotation.Rotation180
+    case Rotation.Rotation300: return Rotation.Rotation240
+  }
+}
